15e7650cf6ee0110d501394916d9405ee85740f5
Kelvin-Kelech <ogkelvin99@gmail.com>
2022-11-05 17:43:56 +0100

    i created models for the mongo db and i also created controllers for the routes, so i am going to set the contollers so i can be able to link it to the routes

---
+++ backend/controllers/BlogController.js
@@ -0,0 +1,13 @@
+const GET_BLOG = () => {};
+const GET_A_BLOG = () => {};
+const POST_BLOG = () => {};
+const DELETE_BLOG = () => {};
+const PATCH_BLOG = () => {};
+
+module.exports = {
+  GET_BLOG,
+  GET_A_BLOG,
+  POST_BLOG,
+  DELETE_BLOG,
+  PATCH_BLOG,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const router = require('./routes/BlogRoute')
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.listen(process.env.PORT, ()=>{
-    console.log('running on port', process.env.PORT)
+mongoose.connect(process.env.MONGO_DB)
+.then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log('connected to database and running on port', process.env.PORT)
+    })
 })
-
+.catch((error)=>{
+    console.log(error)
+}) 
 
 app.use('/api/blog', router)
+
 app.get("/",(req, res)=>{
 res.json({msg: 'hello world'})
-})
+}) 
