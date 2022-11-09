ddca9d1ff141b3ec3e8056dbbc9cb560df1c420d
Kelvin-Kelech <ogkelvin99@gmail.com>
2022-11-09 23:26:22 +0100

    created folder

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="container">  
         <nav>
            <ul>
-            <li style={{float:'left',fontSize:'20px',background:"#04AA6D",paddingRight:'100px'}}><a href="">Domain</a></li>
+            <li style={{float:'left',fontSize:'20px',background:"#04AA6D",paddingRight:'100px'}}><a href="" className="active">Domain</a></li>
             <li><a href="">New post</a></li>
             <li><a href="">About</a></li>
             <li><a href="" className="active"> Home</a></li>
