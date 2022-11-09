f4e2f36f9a0d65f4a76c46885273013c362f8991
Kelvin-Kelech <ogkelvin99@gmail.com>
2022-11-09 21:47:11 +0100

    just finished navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const Navbar = () => {
   return (
     <header>
-      <div className="container">
-        <h2>domain</h2>
+      <div className="container">  
         <nav>
-          <div>
-            <a href="">Home</a>
-            <a href="">New Post</a>
-          </div>
+           <ul>
+            <li style={{float:'left',fontSize:'20px',background:"#04AA6D",paddingRight:'100px'}}><a href="">Domain</a></li>
+            <li><a href="">New post</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="" className="active"> Home</a></li>
+           </ul>
         </nav>
       </div>
     </header>
